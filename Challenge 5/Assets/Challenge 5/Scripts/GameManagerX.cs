b1c360bc79f5c16d446696cdc00e7423d72b2cc9
iammedhat <mido1998152014@gmail.com>
2021-03-02 13:43:45 +0200

    Master

---
+++ Challenge 5/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManagerX : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
+    public Button restartButton; 
+
+    public List<GameObject> targetPrefabs;
+
+    private int score;
+    private float spawnRate = 1.5f;
+    public bool isGameActive;
+
+    private int timeCount;
+
+
+  
+    // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        timeCount = 61;
+        StartCoroutine(TimeCountDown());
+        UpdateScore(0);
+        titleScreen.SetActive(false);
+        
+    }
+
+ 
+    
+    // While game is active spawn a random target
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+
+                Instantiate(targetPrefabs[index]);
+            
+            
+        }
+    }
+
+    IEnumerator TimeCountDown()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(1);
+
+            timeCount--;
+            timeText.text = "Time: " + timeCount;
+
+            if (timeCount == 0)
+            {
+                GameOver();
+            }
+
+            if (timeCount == 10)
+            {
+                timeText.color = Color.red;
+            }
+            
+           
+
+
+        }
+    }
+
+    // Update score with value from target clicked
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "score: " + score;
+    }
+
+    // Stop game, bring up game over text and restart button
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    // Restart game by reloading the scene
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
